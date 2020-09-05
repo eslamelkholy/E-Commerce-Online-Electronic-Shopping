339831bec3032c03fef1a077cb6b0c865a01272f
Eslam Elkholy <eelkholy@qe-software.com>
2020-09-06 01:10:33 +0200

    Setup Database Connection And Environment

---
+++ dist/entities/index.js
@@ -0,0 +1,30 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@mikro-orm/core");
+const dotenv_1 = __importDefault(require("dotenv"));
+const constants_1 = require("./constants");
+dotenv_1.default.config();
+console.log(process.env["DB_NAME"]);
+const main = () => __awaiter(void 0, void 0, void 0, function* () {
+    const orm = yield core_1.MikroORM.init({
+        dbName: process.env.DB_NAME,
+        user: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        type: "postgresql",
+        debug: !constants_1.__PROD__,
+    });
+});
+main();
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Welcome TypeScript2");
+import { MikroORM } from "@mikro-orm/core";
+import dotenv from "dotenv";
+import { __PROD__ } from "./constants";
+dotenv.config();
+
+console.log(process.env["DB_NAME"]);
+const main = async () => {
+  const orm = await MikroORM.init({
+    entities: [],
+    dbName: process.env.DB_NAME,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    type: "postgresql",
+    debug: !__PROD__,
+  });
+};
+
+main();
