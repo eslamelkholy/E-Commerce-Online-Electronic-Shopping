2cc4d77ff6624b1f5a41dab55c5763c09c24e002
Eslam Elkholy <eelkholy@qe-software.com>
2020-09-11 14:04:45 +0200

    Finished ALL Crud Operations Within Item

--- src/resolvers/item.ts
+++ src/resolvers/item.ts
@@ -1,5 +1,5 @@
 import { Item } from "../entities/item";
-import { Resolver, Query, Ctx, Arg, Int } from "type-graphql";
+import { Resolver, Query, Ctx, Arg, Mutation } from "type-graphql";
 import { MyContext } from "../types";
 
 @Resolver()
@@ -10,7 +10,44 @@ export class ItemResolver {
   }
 
   @Query(() => Item, { nullable: true }) //Graphql Type
-  item(@Arg("id", () => Int) id: number, @Ctx() { em }: MyContext): Promise<Item | null> {
+  item(@Arg("id") id: number, @Ctx() { em }: MyContext): Promise<Item | null> {
     return em.findOne(Item, { id });
+  }
+
+  @Mutation(() => Item)
+  async addItem(
+    @Arg("title") title: string,
+    @Arg("description") description: string,
+    @Arg("price") price: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Item> {
+    const newItem = em.create(Item, { title, description, price });
+    await em.persistAndFlush(newItem);
+    return newItem;
+  }
+
+  @Mutation(() => Boolean)
+  async deleteItem(@Arg("id") id: number, @Ctx() { em }: MyContext): Promise<boolean> {
+    const item = await em.findOne(Item, { id });
+    if (!item) return false;
+    await em.nativeDelete(Item, item);
+    return true;
+  }
+
+  @Mutation(() => Item, { nullable: true })
+  async updateItem(
+    @Arg("id") id: number,
+    @Arg("title", () => String, { nullable: true }) title: string,
+    @Arg("description", () => String, { nullable: true }) description: string,
+    @Arg("price", () => Number, { nullable: true }) price: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Item | null> {
+    const item = await em.findOne(Item, { id });
+    if (!item) return null;
+    item.title = title;
+    item.price = price;
+    item.description = description;
+    await em.persistAndFlush(item);
+    return item;
   }
 }
