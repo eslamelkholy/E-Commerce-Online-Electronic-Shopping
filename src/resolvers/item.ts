5d72050eaf2c24c15ef40497cecad5b7ed881a6d
Eslam Elkholy <eelkholy@qe-software.com>
2020-09-11 11:51:26 +0200

    Added Find All Items and Find One Item Resolvers Functionality

--- src/resolvers/item.ts
+++ src/resolvers/item.ts
@@ -1,5 +1,5 @@
 import { Item } from "../entities/item";
-import { Resolver, Query, Ctx } from "type-graphql";
+import { Resolver, Query, Ctx, Arg, Int } from "type-graphql";
 import { MyContext } from "../types";
 
 @Resolver()
@@ -7,5 +7,10 @@ export class ItemResolver {
   @Query(() => [Item!])
   items(@Ctx() { em }: MyContext): Promise<Item[]> {
     return em.find(Item, {});
+  }
+
+  @Query(() => Item, { nullable: true }) //Graphql Type
+  item(@Arg("id", () => Int) id: number, @Ctx() { em }: MyContext): Promise<Item | null> {
+    return em.findOne(Item, { id });
   }
 }
